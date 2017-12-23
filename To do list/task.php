90743052e32f710b23a27f06ee13dca921330a71
Amit Kumar <32258879+geekospot@users.noreply.github.com>
2017-12-23 22:28:03 +0530

    php validated

--- To do list/task.php
+++ To do list/task.php
@@ -13,6 +13,10 @@ if($conn->connect_error)
     }
 
 
+
+if(!empty($_POST["task"]) && !empty($_POST["deadline"]))
+    
+ {    
 $task = $_POST["task"];
 $deadline = $_POST["deadline"];
 $date=date("y/m/d");
@@ -20,20 +24,10 @@ $date=date("y/m/d");
 
 $sql= "insert into task (Task,Date,Deadline) values('$task','$date','$deadline')";
 
-if($conn->query($sql))
-{
-echo $task."<br>";
-echo $deadline."<br>";
-echo $date."<br>";
-}
-else
-{
-echo 'error: '.$sql."<br>".$conn->error;
-    
-}
+$conn->query($sql);
 
-
-
+ }
+ 
 
 header( "refresh:0;url=index.php" );
 
